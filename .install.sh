81c2b80c304ea16b1c7c092574e4e70b807d1566
gaetan <Nuxnuxx>
2023-04-07 05:10:16 +0200

    Open directly font viewer for installing fix

--- .install.sh
+++ .install.sh
@@ -4,7 +4,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-gnome-font-viewer Caskaydia Cove Nerd Font Complete Regular.otf
+gnome-font-viewer Caskaydia\ Cove\ Nerd\ Font\ Complete\ Regular.otf
 read nothing
 
 # Github
