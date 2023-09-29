a8a1b9da1f396f6472ae110b0c74d53e34a5789c
gaetan <Nuxnuxx>
2023-09-29 16:34:33 +0200

    test cht.sh from primagean

--- .scripts/cht.sh
+++ .scripts/cht.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-
-
-languages=$(echo "golang c cpp typescript rust javascript" | tr " " "\n")
-core_utils=$(echo "find xargs sed awk" | tr " " "\n")
-selected=$(echo -e "$languages\n$core_utils" | fzf)
-
-read -p "GIMME YOUR QUERY: " query
-
-if echo "$languages" | grep -qs $selected;then
-	curl cht.sh/$selected/$(echo "$query" | tr " " "+")
-else 
-	curl cht.sh/$selected~$query
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
 fi
 
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
